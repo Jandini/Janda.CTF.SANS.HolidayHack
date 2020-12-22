160a54ba9e9a6c5844a353c50b4089e4ebf49a33
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-22 22:03:47 +0000

    feat: Investigate s3 bucket

---
+++ src/December2020/Challenges/InvestigateS3Bucket.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Net;
+
+namespace Janda.CTF.SANS.HolidayHack
+{
+    [Challenge(Flag = "North Pole: The Frostiest Place on Earth")]
+    public class InvestigateS3Bucket : IChallenge
+    {
+        private readonly ILogger<InvestigateS3Bucket> _logger;       
+
+        public InvestigateS3Bucket(ILogger<InvestigateS3Bucket> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            var client = new WebClient();
+
+            _logger.LogInformation(client.DownloadString("http://s3.amazonaws.com/wrapper3000"));
+
+            using Stream data = client.OpenRead($"http://s3.amazonaws.com/wrapper3000/package");
+            using StreamReader reader = new StreamReader(data);
+            var response = reader.ReadToEnd();
+            _logger.LogInformation(response);
+
+            data.Close();
+            reader.Close();
+        }
+    }
+}
+
+
