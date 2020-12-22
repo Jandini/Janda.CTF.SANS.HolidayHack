e9bc82dbc78dc79f370952c29fff3b502aeb5238
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-22 22:04:11 +0000

    chore: House keeping

--- src/December2020/Program.cs
+++ src/December2020/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Janda.CTF.SANS.HolidayHack
+﻿using Janda.CTF.SANS.HolidayHack.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Janda.CTF.SANS.HolidayHack
 {       
     class Program 
     {
         [CTF(Name = "SANS Holiday Hack, December 2020")]
         static void Main(string[] args) => CTF.Run(args, (services) => services
+            .AddTransient<IS3Scanner, S3Scanner>()
             .AddDictionary());
     }
 }
