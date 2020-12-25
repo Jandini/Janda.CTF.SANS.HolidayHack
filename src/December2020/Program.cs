d2f0fe502df3514374c4a8f078cec353c571af55
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-25 01:33:08 +0000

    feat: Add run in full screen

--- src/December2020/Program.cs
+++ src/December2020/Program.cs
@@ -1,13 +1,28 @@
 ﻿using Janda.CTF.SANS.HolidayHack.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Janda.CTF.SANS.HolidayHack
 {       
     class Program 
     {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static readonly IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int MAXIMIZE = 3;
+
         [CTF(Name = "SANS Holiday Hack, December 2020")]
-        static void Main(string[] args) => CTF.Run(args, (services) => services
-            .AddTransient<IS3Scanner, S3Scanner>()
-            .AddDictionary());
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            CTF.Run(args, (services) => services
+                .AddTransient<IS3Scanner, S3Scanner>()
+                .AddDictionary());
+        }
     }
 }
