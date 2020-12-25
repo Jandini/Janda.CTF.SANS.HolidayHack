748ca8f20efa6a8baff833f862b43446e47b6ad2
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-25 01:34:28 +0000

    chore: Add logging extensions

---
+++ src/December2020/Extensions/LoggingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Janda.CTF
+{
+    public static class LoggingExtensions
+    {       
+        public static T LogMessage<T>(this T o, ILogger logger, string message, params object[] args)
+        {
+            var parameters = new List<object>();
+
+            parameters.AddRange(args);
+            parameters.Add(o);
+
+            logger.LogInformation(message + "\n{@o}", parameters.ToArray());
+
+            return o;
+        }
+    }
+}
