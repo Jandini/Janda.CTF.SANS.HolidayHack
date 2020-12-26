b62974796b58ee1840146adcef608e01610359e9
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-26 22:49:39 +0000

    feat: Complete dialup 33.6kbps challenge

---
+++ src/December2020/Challenges/Floor1/Kitchen/DialUp336Kbps.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Logging;
+
+namespace Janda.CTF.SANS.HolidayHack
+{
+    public class DialUp336Kbps : IChallenge
+    {
+        private readonly ILogger<DialUp336Kbps> _logger;
+
+        public DialUp336Kbps(ILogger<DialUp336Kbps> logger)
+        {
+            _logger = logger;
+        }
+        
+        public void Run()
+        {
+            @"
+                We can shuffle the colors of the lights by connecting via dial-up, but our only modem is broken!
+
+                Fortunately, I speak dial-up. However, I can't quite remember the handshake sequence.
+
+                Maybe you can help me out? The phone number is 756-8347; you can use this blue phone.
+
+                ...
+
+                ""Put it in the cloud,"" they said...
+
+            ".LogMessage(_logger, "Fitzy Shortstack");
+
+            // ["pickup","dtmf7","dtmf5","dtmf6","dtmf8","dtmf3","dtmf4","dtmf7", "respCrEsCl","ack","cm_cj","l1_l2_info","trn"].forEach((step)=> document.getElementsByClassName(step)[0].click());
+            @"[""pickup"",""dtmf7"",""dtmf5"",""dtmf6"",""dtmf8"",""dtmf3"",""dtmf4"",""dtmf7"", ""respCrEsCl"",""ack"",""cm_cj"",""l1_l2_info"",""trn""].forEach((step)=> document.getElementsByClassName(step)[0].click());"
+                .LogMessage(_logger, "Run from console to call");
+        }
+    }
+}
