c05b41c05c985c735f7a8427f6b605fe1f1b3fcb
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-27 12:31:08 +0000

    chore: Add elf chats

---
+++ src/December2020/Resources/getElfChat.js
@@ -0,0 +1,7 @@
+﻿// get elf text
+setInterval(() => { console.log(document.getElementsByClassName("npc-username")[0].outerText); let e = document.getElementsByClassName("chat"); if (e && e.length) console.log(e[0].outerText); }, 1000);
+
+// get elf name
+document.getElementsByClassName("npc-username")[0]
+
+
