a33862fd1065c0b7d97f3758bac6d43c7a50a773
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-29 17:36:01 +0000

    chore: Update block chain investigation

--- src/December2020/Challenges/Floor3/SantasOffice/BlockchainInvestigationPart2.cs
+++ src/December2020/Challenges/Floor3/SantasOffice/BlockchainInvestigationPart2.cs
@@ -11,7 +11,17 @@ namespace Janda.CTF.SANS.HolidayHack
         {
             _logger = logger;
         }
-        
+
+        public void Objective()
+        {
+            @"
+                The SHA256 of Jack's altered block is: 58a3b9335a6ceb0234c12d35a0564c4e f0e90152d0eb2ce2082383b38028a90f. 
+                If you're clever, you can recreate the original version of that block by changing the values of only 4 bytes. 
+                Once you've recreated the original block, what is the SHA256 of that block?
+
+            ".Log(_logger);
+        }
+
         public void Run()
         {
 
