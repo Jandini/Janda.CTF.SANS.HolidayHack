5a7db320afd7e3078fa79b4125c6606d866125f4
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-29 14:29:26 +0000

    chore: Housekeeping

--- src/December2020/Challenges/Floor3/SantasOffice/BlockchainInvestigationPart1.cs
+++ src/December2020/Challenges/Floor3/SantasOffice/BlockchainInvestigationPart1.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Janda.CTF.SANS.HolidayHack
 {
+    [Challenge(Name = "Objective 11a: Naughty/Nice List with Blockchain Investigation Part 1", Points = 5)]
     public class BlockchainInvestigationPart1 : IChallenge
     {
         private readonly ILogger<BlockchainInvestigationPart1> _logger;
@@ -11,6 +12,19 @@ namespace Janda.CTF.SANS.HolidayHack
             _logger = logger;
         }
         
+
+        public void Objective()
+        {
+            @"
+                Even though the chunk of the blockchain that you have ends with block 129996, can you predict the nonce for block 130000? 
+                Talk to Tangle Coalbox in the Speaker UNpreparedness Room for tips on prediction and Tinsel Upatree for more tips and tools. 
+
+                (Enter just the 16-character hex value of the nonce)
+
+            ".Log(_logger);
+        }
+
+
         public void Run()
         {
 
