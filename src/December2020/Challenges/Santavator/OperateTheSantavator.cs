3f5add19d1e77f3406daa3e59aab0afbcb5f193c
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-29 14:29:17 +0000

    feat: Add operate the santavator challenge

---
+++ src/December2020/Challenges/Santavator/OperateTheSantavator.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Logging;
+
+namespace Janda.CTF.SANS.HolidayHack
+{
+    public class OperateTheSantavator : IChallenge
+    {
+        private readonly ILogger<OperateTheSantavator> _logger;
+
+        public OperateTheSantavator(ILogger<OperateTheSantavator> logger)
+        {
+            _logger = logger;
+        }
+        
+        public void Run()
+        {
+            @"
+
+                <div class=""cover"">
+                    <div class=""localstorage-error""><strong>Heads up:</strong> Your Santavator repair configuration cannot be accessed or saved in incognito mode.</div>
+                    <img class=""f15btn found"" src=""images/floor1-5button.png"">
+                    <div class=""key""></div>
+                    <div class=""print-cover""></div>
+                    <button class=""btn btn1 active powered"" data-floor=""1"">1</button>
+                    <button class=""btn btn15"" data-floor=""1.5"">1.5</button>`
+                    <button class=""btn btn2 powered"" data-floor=""2"">2</button>
+                    <button class=""btn btn3"" data-floor=""3"">3</button>
+                    <button class=""btn btnr"" data-floor=""r"">R</button>
+                </div>
+
+            ".Log(_logger, "Access panel without key... {answer}", "Hide the cover html element");
+
+        }
+    }
+}
